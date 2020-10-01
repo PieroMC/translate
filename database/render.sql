a01304c348707d33d64bc8f772a36d82d1643a30
Pxndx-user007 <cmerino@unitru.edu.pe>
2020-09-30 22:38:07 -0500

    update render.sql

--- database/render.sql
+++ database/render.sql
@@ -1,13 +1,15 @@
 use db_translate;
-INSERT INTO `words` (`id`, `word`, `translated`, `created_at`) VALUES
-(1, 'CABALLO', 'HORSE', '2020-09-29 07:27:33'),
-(2, 'CASA', 'HOME', '2020-09-30 03:39:06'),
-(3, 'ROJO', 'RED', '2020-09-30 03:56:12'),
-(4, 'LADY', 'DAMA', '2020-09-30 04:06:11'),
-(5, 'BREAK', 'DESCANSO', '2020-09-30 04:06:35'),
-(6, 'SEED', 'SEMILLA', '2020-09-30 04:06:51'),
-(7, 'CLEAN', 'LIMPIAR', '2020-09-30 04:07:13'),
-(8, 'TEAM', 'EQUIPO', '2020-09-30 04:07:36'),
-(9, 'WOMAN', 'MUJER', '2020-09-30 04:07:49'),
-(10, 'HIT', 'GOLPEAR', '2020-09-30 04:08:03'),
-(11, 'HISTORY', 'HISTORIA', '2020-09-30 04:08:13');
+INSERT INTO
+  `words` (`id`, `word`, `translated`, `created_at`)
+VALUES
+  (1, 'CABALLO', 'HORSE', '2020-09-29 07:27:33'),
+  (2, 'CASA', 'HOME', '2020-09-30 03:39:06'),
+  (3, 'ROJO', 'RED', '2020-09-30 03:56:12'),
+  (4, 'DAMA', 'LADY', '2020-09-30 04:06:11'),
+  (5, 'DESCANSO', 'BREAK', '2020-09-30 04:06:35'),
+  (6, 'SEMILLA', 'SEED', '2020-09-30 04:06:51'),
+  (7, 'LIMPIAR', 'CLEAN', '2020-09-30 04:07:13'),
+  (8, 'EQUIPO', 'TEAM', '2020-09-30 04:07:36'),
+  (9, 'MUJER', 'WOMAN', '2020-09-30 04:07:49'),
+  (10, 'GOLPEAR', 'HIT', '2020-09-30 04:08:03'),
+  (11, 'HISTORIA', 'HISTORY', '2020-09-30 04:08:13');
