efa05db3faf80d1c0fcb4e24f47dd7a150457c50
Pxndx-user007 <cmerino@unitru.edu.pe>
2020-09-30 21:50:38 -0500

    translate CRUD finished

--- server/build/controller/translateController.js
+++ server/build/controller/translateController.js
@@ -1,29 +1,64 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = __importDefault(require("../database"));
 class TranslateContoller {
     /**
      * show @void
      */
     show(req, res) {
-        res.json({ text: "these are all the words" });
+        return __awaiter(this, void 0, void 0, function* () {
+            const words = yield database_1.default.query('SELECT * FROM words');
+            if (words.length > 0) {
+                res.json([{ message: "success", words }]);
+            }
+            else {
+                res.json({ message: "not found" });
+            }
+        });
     }
     /**
      * showOne @void
      */
     showOne(req, res) {
-        res.json({ text: "this is a word " + req.params.word });
+        return __awaiter(this, void 0, void 0, function* () {
+            const word = yield database_1.default.query('SELECT * FROM words WHERE word = ?', req.params.word);
+            if (word.length > 0) {
+                res.json([{ message: "success", word }]);
+            }
+            else {
+                res.json({ message: "not found" });
+            }
+        });
     }
     /**
      * create @void
      */
     create(req, res) {
-        res.json({ text: "creating a word" });
+        return __awaiter(this, void 0, void 0, function* () {
+            yield database_1.default.query('INSERT INTO words set ?', [req.body]);
+            res.json({ message: "success" });
+        });
     }
     /**
      * delete @void
      */
     delete(req, res) {
-        res.json({ text: "deleting a word " + req.params.id });
+        return __awaiter(this, void 0, void 0, function* () {
+            yield database_1.default.query('DELETE FROM words WHERE id = ?', req.params.id);
+            res.json({ message: "success" });
+        });
     }
 }
 const translateController = new TranslateContoller();
--- server/build/database.js
+++ server/build/database.js
@@ -6,8 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const promise_mysql_1 = __importDefault(require("promise-mysql"));
 const env_1 = __importDefault(require("./env"));
 const pool = promise_mysql_1.default.createPool(env_1.default.database);
-pool.get("releaseConnection")
+pool.getConnection()
     .then(connection => {
-    console.log('DataBase is Connected');
+    pool.releaseConnection(connection);
+    console.log('DB is Connected');
 });
 exports.default = pool;
